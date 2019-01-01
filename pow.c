fe0e69e30720c8d07b4d2227b27b643c8a266c0f
JavaCuber <35405030+JavaCuber@users.noreply.github.com>
2019-01-01 22:34:00 +0100

    Add files via upload

---
+++ pow.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <gmp.h>
+#include <math.h>
+
+int main(){
+  // compiling: clang pow.c -o pow -lgmp
+  printf("Computing...\n");
+
+  mpz_t res;
+  mpz_init(res);
+  mpz_ui_pow_ui(res,9,pow(9,9));
+
+  printf("Writing...\n");
+
+  FILE *ptr;
+  ptr = fopen("ninetoninetonine.txt","w+");
+  gmp_fprintf(ptr, "%Zd", res);
+
+  mpz_clear(res);
+
+  return 0;
+}
